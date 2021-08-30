7787323b67998bb65bf80d1e79940dece1be612d
han-jungwon <rhehf1024@naver.com>
2021-08-30 23:26:49 +0900

    회원등록까지

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+  private final EntityManager em;
+
+  public void save(Order order) {
+    em.persist(order);
+  }
+
+  public Order findOne(Long id) {
+    return em.find(Order.class, id);
+  }
+
+  public List<Member> findByName(String name) {
+    return em.createQuery("select m from Member m where m.name = :name",
+      Member.class)
+      .setParameter("name", name)
+      .getResultList();
+  }
+}
